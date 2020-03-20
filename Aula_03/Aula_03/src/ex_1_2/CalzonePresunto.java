a29ddf57240edabbbd28c7af923ff66f8bf6dbf5
paulosgiovannini <paulosgiovannini@icloud.com>
2020-03-20 15:26:56 -0300

    Versão 3.1

---
+++ Aula_03/Aula_03/src/ex_1_2/CalzonePresunto.java
@@ -0,0 +1,9 @@
+package ex_1_2;
+
+public class CalzonePresunto extends Calzone {
+
+	@Override
+	public String obterIngredientes() {
+		return "Queijo, Calabresa e Tomate.";
+	}
+}
---
+++ Aula_03/Aula_03/src/ex_1_2/Pizza.java
@@ -0,0 +1,1 @@
+package ex_1_2;public abstract class Pizza {	public abstract String obterIngredientes();}
