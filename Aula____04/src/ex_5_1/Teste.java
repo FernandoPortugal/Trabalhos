0702b743ba9d64bd9c1267918c5c554c01045ebc
paulosgiovannini <paulosgiovannini@icloud.com>
2020-04-17 20:48:55 -0300

    so o exercicio 5.1

---
+++ Aula____04/src/ex_5_1/Teste.java
@@ -0,0 +1,14 @@
+
+package ex_5_1;
+
+public class Teste {
+	
+public static void main(String[] args) {
+		
+		IOFacade io = new IOFacade();		
+		io.lerTxt();
+		io.lerBinario();
+		io.lerObjeto();
+
+	}
+}
